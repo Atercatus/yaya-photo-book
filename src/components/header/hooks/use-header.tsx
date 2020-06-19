32a50ec51f168793ec671cfbee26dd4641c98eec
Atercatus <myleelee2053@naver.com>
2020-06-19 16:15:45 +0900

    add logic

--- src/components/header/hooks/use-header.tsx
+++ src/components/header/hooks/use-header.tsx
@@ -1,3 +1,9 @@
-const useHeader = () => {};
+const useHeader = () => {
+  window.addEventListener('scroll', () => {
+    console.log('scroll');
+    console.log('test');
+    const a = 10;
+  });
+};
 
 export default useHeader;
